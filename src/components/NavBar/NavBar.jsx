5504d89fd7f9dbfa161af38452864ebe137b8828
Franklin Ohaegbulam <46662771+frankiefab100@users.noreply.github.com>
2024-12-16 17:11:52 +0100

    Update NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,7 @@ import Hamburger from "/icons/Hamburger-menu.svg";
 import Close from "/icons/Close icon.svg";
 import Logo from "/logo/Logo-coloured.png";
 import LogoMobile from "/logo/Logo-mobile.png";
-import "../styles/scss/components/_navbar.scss";
+import "../styles/sass/_navbar.scss";
 
 const NavBar = () => {
   const [showMenu, setShowMenu] = useState(false);
